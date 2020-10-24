5adcbf1d7fa5b8aa1ea22adda3883f050de477d9
Gabby Miller <gabriellaeakin@gmail.com>
2020-10-24 19:41:44 -0400

    updated 5 day forecast

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # weather-application
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I open the weather dashboard
+THEN I am presented with the last searched city forecast
+
+In my weather application, I used 2 API's from openweathermap.org. I used the current weather data and the 5day/3 hour forecast API's. In javascript, I created 2 ajax calls for each of my API's and dynamically appended content to my HTML using JQuery. 
+
+When you type a city into the search bar, it prepends beneath the button to show the most recently searched city first, and then lists out the other searched cities. When a city is searched, it is pulled into my "city" variable in JS, which is inserted in my ajax URL. This ensures that the proper data is showing on the page. 
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
 // function to run ajax for 5 day forecast 
-
-
 function weatherforecast (){
 
 // defining "city" by grabbing the value inside of the city-search input field 
@@ -17,17 +15,14 @@ function weatherforecast (){
     
     //    console.log(response);
 
-
+// forecast for tomorrow 
     var card1 = $("#card1");
-
-    var tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-
-    var p = $("<p>").text(tomorrow);
-    card1.append(p);
-
+// tomorrow's date 
+    var tomorrow= moment().add(1,'day').format('MM/DD/YYYY');
+    var tom = $("<h4>").text(tomorrow);
+    card1.append(tom);
 // path to get icon code 
-        var iconcode1 = (response.list[0].weather[0].icon);
+        var iconcode1 = (response.list[3].weather[0].icon);
 // path for img src 
         var iconurl = "http://openweathermap.org/img/wn/" + iconcode1 + ".png";
 // creating an img tag 
@@ -36,15 +31,124 @@ function weatherforecast (){
         img1.attr("src", iconurl);
 // appending img 1 to my card 1
         card1.append(img1);
+// temp card for tomorrow 
+        var temp=(response.list[3].main.temp); 
+        var temptomorrow = $("<p>").text("Temp:" + temp + "°F");
+        temptomorrow.addClass("tomorrowclass");
+        card1.append(temptomorrow);
+        var hum = (response.list[3].main.humidity);
+        var humtomorrow = $("<p>").text("Humidity:" + hum + "%");
+        humtomorrow.addClass("tomorrowclass");
+        card1.append(humtomorrow);
         
-        // console.log(response.list[0].weather[0]);
+        console.log(response);
+// 2nd day forecast 
+        var card2=$("#card2");
+        // 2 day forecast
+        var twoday= moment().add(2,'day').format('MM/DD/YYYY');
+        var twodays = $("<h4>").text(twoday);
+        card2.append(twodays);
+// path to get icon code 
+        var iconcode2 = (response.list[8].weather[0].icon);
+// path for img src 
+        var iconurl = "http://openweathermap.org/img/wn/" + iconcode2 + ".png";
+// creating an img tag 
+        var img2 = $("<img>");
+// setting img attr - src to iconurl
+        img2.attr("src", iconurl);
+// appending img 1 to my card 1
+        card2.append(img2);
+// temp card for tomorrow 
+        var temp2=(response.list[8].main.temp); 
+        var temptwoday = $("<p>").text("Temp:" + temp2 + "°F");
+        temptwoday.addClass("tomorrowclass");
+        card2.append(temptwoday);
+        var hum2 = (response.list[8].main.humidity);
+        var humtwo = $("<p>").text("Humidity:" + hum2 + "%");
+        humtwo.addClass("tomorrowclass");
+        card2.append(humtwo);
 
-        console.log(response.list[0].weather[0].icon);
+// 3rd day forecast  
 
+var card3=$("#card3");
+        // 2 day forecast
+        var threeday= moment().add(3,'day').format('MM/DD/YYYY');
+        var threedays = $("<h4>").text(threeday);
+        card3.append(threedays);
+// path to get icon code 
+        var iconcode3 = (response.list[18].weather[0].icon);
+// path for img src 
+        var iconurl = "http://openweathermap.org/img/wn/" + iconcode3 + ".png";
+// creating an img tag 
+        var img3 = $("<img>");
+// setting img attr - src to iconurl
+        img3.attr("src", iconurl);
+// appending img 1 to my card 1
+        card3.append(img3);
+// temp card for tomorrow 
+        var temp3=(response.list[18].main.temp); 
+        var tempthreeday = $("<p>").text("Temp:" + temp3 + "°F");
+        tempthreeday.addClass("tomorrowclass");
+        card3.append(tempthreeday);
+        var hum3 = (response.list[13].main.humidity);
+        var humthree = $("<p>").text("Humidity:" + hum3 + "%");
+        humthree.addClass("tomorrowclass");
+        card3.append(humthree);
+
+// 4th day forecast 
+var card4=$("#card4");
+        // 2 day forecast
+        var fourday= moment().add(4,'day').format('MM/DD/YYYY');
+        var fourdays = $("<h4>").text(fourday);
+        card4.append(fourdays);
+// path to get icon code 
+        var iconcode4 = (response.list[24].weather[0].icon);
+// path for img src 
+        var iconurl = "http://openweathermap.org/img/wn/" + iconcode4 + ".png";
+// creating an img tag 
+        var img4 = $("<img>");
+// setting img attr - src to iconurl
+        img4.attr("src", iconurl);
+// appending img 1 to my card 1
+        card4.append(img4);
+// temp card for tomorrow 
+        var temp4=(response.list[24].main.temp); 
+        var tempfourday = $("<p>").text("Temp:" + temp4 + "°F");
+        tempfourday.addClass("tomorrowclass");
+        card4.append(tempfourday);
+        var hum4 = (response.list[24].main.humidity);
+        var humfour = $("<p>").text("Humidity:" + hum4 + "%");
+        humfour.addClass("tomorrowclass");
+        card4.append(humfour);
+
+// 5th day forecast 
+var card5=$("#card5");
+        // 2 day forecast
+        var fiveday= moment().add(5,'day').format('MM/DD/YYYY');
+        var fivedays = $("<h4>").text(fiveday);
+        card5.append(fivedays);
+// path to get icon code 
+        var iconcode5 = (response.list[34].weather[0].icon);
+// path for img src 
+        var iconurl = "http://openweathermap.org/img/wn/" + iconcode5 + ".png";
+// creating an img tag 
+        var img5 = $("<img>");
+// setting img attr - src to iconurl
+        img5.attr("src", iconurl);
+// appending img 1 to my card 1
+        card5.append(img5);
+// temp card for tomorrow 
+        var temp5=(response.list[34].main.temp); 
+        var tempfiveday = $("<p>").text("Temp:" + temp5 + "°F");
+        tempfiveday.addClass("tomorrowclass");
+        card5.append(tempfiveday);
+        var hum5 = (response.list[34].main.humidity);
+        var humfive = $("<p>").text("Humidity:" + hum5 + "%");
+        humfive.addClass("tomorrowclass");
+        card5.append(humfive);
     });
 
 };
-
 
 // function to run ajax call for current weather 
 function currentweather () {
@@ -92,6 +196,11 @@ function currentweather () {
 $(".button").on("click", function(event){
     event.preventDefault(); 
     $("#display-weather").empty(); 
+    $("#card1").empty(); 
+    $("#card2").empty(); 
+    $("#card3").empty(); 
+    $("#card4").empty(); 
+    $("#card5").empty(); 
     weatherforecast(); 
     currentweather(); 
 
